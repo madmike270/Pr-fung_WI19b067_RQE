1ccab833451fd16ac8bbe75e799d19b7576e3408
Mike <m.fahrafellner@hotmail.com>
2020-11-02 18:42:56 +0100

    initial commit

---
+++ Fahrafellner_Test/Fahrafellner_Test/SpecFlowFeature1.feature
@@ -0,0 +1,34 @@
+﻿Feature: SpecFlowFeature1
+
+ In order to avoid silly mistakes
+ As a mathematician
+ I want to be told the result of specific operations
+
+@mytag
+
+Scenario: CalcPi
+
+ Given null
+ Then pi is returned
+
+ Scenario: CalcSin
+
+ Given the first number is 50
+ Then sin is -0.26
+
+
+Scenario: CalcCos
+
+ Given the first number is 50
+ Then Cos is 0.96
+
+
+
+Scenario: CalcTan
+
+ Given the first number is 50
+
+ Then tan is -0.27
+
+
+
---
+++ Fahrafellner_Test/Fahrafellner_Test/SpecFlowFeature1Steps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Fahrafellner_Test
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+
+        [Given(@"null")]
+        public void GivenNull()
+        { 
+
+        }
+        
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"pi is returned")]
+        public void ThenPiIsReturned()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"sin is (.*)")]
+        public void ThenSinIs(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Cos is (.*)")]
+        public void ThenCosIs(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"tan is (.*)")]
+        public void ThenTanIs(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
